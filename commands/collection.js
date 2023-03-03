b66e145061d6f441945aec0b7f09879e47b83a1f
Ryan Henderson <ryanehenderson@protonmail.com>
2023-03-02 23:14:17 -0600

    Implement basic card adding functionality

--- commands/collection.js
+++ commands/collection.js
@@ -37,42 +37,215 @@ module.exports = {
             subcommand
                 .setName('delete')
                 .setDescription('Deletes your collection permanently')
+        )
+        .addSubcommandGroup(group =>
+            group
+                .setName('card')
+                .setDescription('Manages adding and removing cards from the collection')
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName('add')
+                        .setDescription('Adds a card to the collection')
+                        .addStringOption(option =>
+                            option
+                                .setName('name')
+                                .setDescription('The name of the card')
+                                .setRequired(true)    
+                        )
+                        .addStringOption(option =>
+                            option
+                                .setName('set')
+                                .setDescription('The set code of the card')
+                                .setRequired(true)    
+                        )
+                        .addBooleanOption(option =>
+                            option
+                                .setName('foil')
+                                .setDescription('Whether or not the card is foil (default false)')    
+                        )
+                        .addBooleanOption(option =>
+                            option
+                                .setName('token')
+                                .setDescription('Whether the card is a token (default false)')    
+                        )
+                        .addIntegerOption(option =>
+                            option
+                                .setName('count')
+                                .setDescription('How many of the card to add (default 1)')    
+                        )
+                        .addStringOption(option =>
+                            option
+                                .setName('location')
+                                .setDescription('Location the card is for organization (default none)')
+                        )
+                )
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName('remove')
+                        .setDescription('Removes a card from the collection')
+                        .addStringOption(option =>
+                            option
+                                .setName('name')
+                                .setDescription('The name of the card')
+                                .setRequired(true)    
+                        )
+                        .addStringOption(option =>
+                            option
+                                .setName('set')
+                                .setDescription('The set code of the card')
+                                .setRequired(true)    
+                        )
+                        .addBooleanOption(option =>
+                            option
+                                .setName('foil')
+                                .setDescription('Whether or not the card is foil (default false)')    
+                        )
+                        .addBooleanOption(option =>
+                            option
+                                .setName('token')
+                                .setDescription('Whether the card is a token (default false)')    
+                        )
+                        .addIntegerOption(option =>
+                            option
+                                .setName('count')
+                                .setDescription('How many of the card to remove (default 1)')    
+                        )
+                        .addStringOption(option =>
+                            option
+                                .setName('location')
+                                .setDescription('Location the card is for organization (default none)')
+                        )
+                )
+                .addSubcommand(subcommand =>
+                    subcommand
+                        .setName('move')
+                        .setDescription('Moves a card from one location to another')
+                        .addStringOption(option =>
+                            option
+                                .setName('name')
+                                .setDescription('The name of the card')
+                                .setRequired(true)    
+                        )
+                        .addStringOption(option =>
+                            option
+                                .setName('set')
+                                .setDescription('The set code of the card')
+                                .setRequired(true)    
+                        )
+                        .addStringOption(option =>
+                            option
+                                .setName('newlocation')
+                                .setDescription('New location of the card (\'null\' for no location)')
+                                .setRequired(true)
+                        )
+                        .addStringOption(option =>
+                            option
+                                .setName('oldlocation')
+                                .setDescription('Old location of the card (default none)')
+                        )
+                        .addBooleanOption(option =>
+                            option
+                                .setName('foil')
+                                .setDescription('Whether or not the card is foil (default false)')    
+                        )
+                        .addBooleanOption(option =>
+                            option
+                                .setName('token')
+                                .setDescription('Whether the card is a token (default false)')    
+                        )
+                        .addIntegerOption(option =>
+                            option
+                                .setName('count')
+                                .setDescription('How many of the card to move (default 1)')    
+                        )
+                )
         ),
     async execute(interaction) {
-        const subcommand = interaction.options._subcommand;
+        const subcommand = interaction.options.getSubcommand();
+        const group = interaction.options.getSubcommandGroup();
         const user = interaction.user;
         await interaction.deferReply({ ephemeral: true });
 
-        if (subcommand === 'init') {
-            let tableExists = await hasTable(user.id);
-            if (tableExists) {
-                interaction.editReply('You already have an existing collection');
-            } else {
-                createTable(user.id).then(() => {
-                    interaction.editReply('Your collection has been created');
-                }).catch((err) => {
-                    interaction.editReply('An error occurred creating your collection');
-                    console.err(err);
-                })
-            }
-        } else if (subcommand === 'delete') {
-            let tableExists = await hasTable(user.id);
+        const tableExists = await hasTable(user.id);
+        if (group === 'card') {
             if (!tableExists) {
-                interaction.editReply('You do not have a collection to delete');
-            } else {
-                const embed = createDeleteEmbed();
-                const buttons = createConfirmationButtons(user.id);
-                interaction.editReply({ embeds: [embed], components: [buttons] });
+                interaction.editReply('You do not have a collection started. Run `/collection init` to start one');
+                return;
+            }
+            const cardName = interaction.options.getString('name');
+            const cardSet = interaction.options.getString('set');
+            const isFoil = interaction.options.getBoolean('foil') === true ? true : false;
+            const isToken = interaction.options.getBoolean('token') === true ? true : false;
+            const count = interaction.options.getInteger('count') === null ? 1 : interaction.options.getInteger('count');
 
-                setTimeout(() => {
-                    interaction.fetchReply().then((reply) => {
-                        if (reply.embeds[0].data.title === 'Are you sure?') {
-                            const newEmbed = createTimeoutDeleteEmbed();
-                            const newButtons = createDisabledConfirmationButtons(user.id);
-                            interaction.editReply({ embeds: [newEmbed], components: [newButtons] });
+            if (subcommand === 'add' || subcommand === 'remove') {
+                const location = interaction.options.getString('location');
+                getCard(cardName, cardSet, isToken).then((cards) => {
+                    console.log(cards);
+                    if (cards.length === 0) {
+                        interaction.editReply(`Could not find a card with name \`${cardName}\` in set \`${cardSet}\``);
+                        return;
+                    }
+                    if (cards.length === 1) {
+                        const card = cards[0];
+                        if (card.name !== cardName) {
+                            const embed = createNameVerificationEmbed(card.name, cardName);
+                            const buttons = createConfirmationButtons(user.id);
+                            interaction.editReply({ embeds: [embed], components: [buttons] });
+                        } else {
+                            const hasFoil = card.hasFoil;
+                            const hasNonFoil =  card.hasNonFoil;
+                            if (isFoil && !hasFoil) {
+                                interaction.editReply('Foil card was selected, but this card does not have a foil version');
+                            } else if (!isFoil && !hasNonFoil) {
+                                interaction.editReply('Non-foil card was selected, but this card only exists in foil');
+                            } else {
+                                if (subcommand === 'add') {
+                                    addCard(user.id, card.uuid, isFoil, count, location).then(() => {
+                                        interaction.editReply('Card added');
+                                    }).catch((err) => {
+                                        interaction.editReply('An error occurred adding the card');
+                                        console.log(err);
+                                    });
+                                }
+                            }
                         }
-                    });
-                }, 15000);
+                    }
+                }).catch((err) => {
+                    interaction.editReply('An error occurred finding that card');
+                    console.log(err);
+                });
+            }
+        } else if (group === null) {
+            if (subcommand === 'init') {
+                if (tableExists) {
+                    interaction.editReply('You already have an existing collection');
+                } else {
+                    createTable(user.id).then(() => {
+                        interaction.editReply('Your collection has been created');
+                    }).catch((err) => {
+                        interaction.editReply('An error occurred creating your collection');
+                        console.log(err);
+                    })
+                }
+            } else if (subcommand === 'delete') {
+                if (!tableExists) {
+                    interaction.editReply('You do not have a collection to delete');
+                } else {
+                    const embed = createDeleteEmbed();
+                    const buttons = createConfirmationButtons(user.id);
+                    interaction.editReply({ embeds: [embed], components: [buttons] });
+    
+                    setTimeout(() => {
+                        interaction.fetchReply().then((reply) => {
+                            if (reply.embeds[0].data.title === 'Are you sure?') {
+                                const newEmbed = createTimeoutDeleteEmbed();
+                                const newButtons = createDisabledConfirmationButtons(user.id);
+                                interaction.editReply({ embeds: [newEmbed], components: [newButtons] });
+                            }
+                        });
+                    }, 15000);
+                }
             }
         }
     },
@@ -100,23 +273,55 @@ async function hasTable(userId) {
     }
 }
 
+// Each user's collection only needs the card UUID and then data not stored in the mtgjson data
+// All other information about the card will be dynamically pulled from mtgjson database
 async function createTable(userId) {
     let conn;
     try {
         conn = await pool.getConnection();
         const newTable = await conn.query(
             `CREATE TABLE IF NOT EXISTS u${userId} (
-                card_name VARCHAR(200) NOT NULL,
-                card_set VARCHAR(10) NOT NULL,
+                uuid CHAR(36) NOT NULL,
                 foil BOOLEAN NOT NULL,
                 count SMALLINT NOT NULL,
-                location VARCHAR(100),
-                price VARCHAR(10),
-                colors SET('W', 'U', 'B', 'R', 'G', 'C') NOT NULL,
-                rarity CHAR(1) NOT NULL,
-                id VARCHAR(128) NOT NULL
+                location VARCHAR(100)
               )`
         );
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) {
+            return conn.end();
+        }
+    }
+}
+
+async function getCard(cardName, setCode, token) {
+    let conn;
+    let card;
+    try {
+        conn = await pool.getConnection();
+        if (token) {
+            card = await conn.query(`SELECT * FROM tokens WHERE (name LIKE '%${cardName}%') AND (setCode='${setCode}')`);
+        } else {
+            card = await conn.query(`SELECT * FROM cards WHERE (name LIKE '%${cardName}%') AND (setCode='${setCode}')`);
+        }
+        
+    } catch (err) {
+        throw err;
+    } finally {
+        if (conn) {
+            conn.end();
+            return card;
+        }
+    }
+}
+
+async function addCard(userId, uuid, foil, count, location) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        added = await conn.query(`INSERT INTO u${userId} VALUES (?, ?, ?, ?)`, [uuid,  foil, count, location]);
     } catch (err) {
         throw err;
     } finally {
@@ -169,5 +374,16 @@ function createTimeoutDeleteEmbed() {
     const embed = new EmbedBuilder()
         .setTitle('Deletion timed out')
         .setDescription('Collection deletion has timed out');
+    return embed;
+}
+
+function createNameVerificationEmbed(foundName, givenName) {
+    const embed = new EmbedBuilder()
+        .setTitle('Name Discrepancy')
+        .setDescription('Found a card with a similar name, is this correct?')
+        .addFields(
+            { name: 'Provided Name', value: givenName },
+            { name: 'Found Name', value: foundName }
+        );
     return embed;
 }
